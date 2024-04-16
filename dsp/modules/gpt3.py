0536661b2cc9eba34b1a381beed79382fd1dc34e
Tanmay Garg <tanmay17061@gmail.com>
2024-04-16 08:03:02 +0530

    GPT3: increase max_tokens

--- dsp/modules/gpt3.py
+++ dsp/modules/gpt3.py
@@ -86,7 +86,7 @@ class GPT3(LM):
 
         self.kwargs = {
             "temperature": 0.0,
-            "max_tokens": 150,
+            "max_tokens": 500,
             "top_p": 1,
             "frequency_penalty": 0,
             "presence_penalty": 0,
